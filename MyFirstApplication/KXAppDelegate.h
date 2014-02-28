c1516e0a7776c8233af6201cc5621f0f7f9cc230
Kenneth Wilcox <ken@KenMacVM.local>
2014-02-28 13:03:50 -0800

    Initial Commit

---
+++ MyFirstApplication/KXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KXAppDelegate.h
+//  MyFirstApplication
+//
+//  Created by Kenneth Wilcox on 2/28/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFirstApplication/KXViewController.h
@@ -0,0 +1,13 @@
+//
+//  KXViewController.h
+//  MyFirstApplication
+//
+//  Created by Kenneth Wilcox on 2/28/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXViewController : UIViewController
+
+@end
